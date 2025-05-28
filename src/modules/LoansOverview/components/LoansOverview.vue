90dd78505f5d2bf3c38233b56d78a191e1d4cd97
Alexander_721 <alexander3post@gmail.com>
2025-05-28 15:13:41 +0300

    add section active loans overview

--- src/modules/LoansOverview/components/LoansOverview.vue
+++ src/modules/LoansOverview/components/LoansOverview.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import BaseTitle from '@/shared/components/BaseTitle.vue'
 import LoansFeatures from './LoansOverviewFeatures.vue'
+import LoansOverviewTable from './LoansOverviewTable.vue';
 </script>
 
 <template>
     <section aria-label="Loans">
         <LoansFeatures />
         <BaseTitle text="Active Loans Overview" />
+        <LoansOverviewTable />
     </section>
 </template>
--- src/modules/LoansOverview/components/LoansOverviewFeatures.vue
+++ src/modules/LoansOverview/components/LoansOverviewFeatures.vue
@@ -19,18 +19,18 @@ const content = [
         descr: '$500,000',
         icon: 'up',
     },
-    {
-        id: 4,
-        title: 'Custom Loans',
-        descr: 'Choose Money',
-        icon: 'settings',
-    },
+    // {
+    //     id: 4,
+    //     title: 'Custom Loans',
+    //     descr: 'Choose Money',
+    //     icon: 'settings',
+    // },
 ]
 
 </script>
 
 <template>
-    <div class="scrollable mb-5 flex justify-between gap-5 overflow-x-auto xl:overflow-hidden xl:flex-row">
+    <div class="scrollable mb-6 lg:mb-10 flex justify-between gap-5 overflow-x-auto xl:overflow-hidden xl:flex-row">
         <FeatureCard v-for="item in content" :key="item.id" :title="item.title" :descr="item.descr" :icon="`modules-LoansOverview-icons-${item.icon}`" />
     </div>
 </template>
